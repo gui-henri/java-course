50daf9cbcf7cc2ff220237191af7dec05b203e7a
Astrobio <astrobio@pop-os.localdomain>
2021-09-30 22:37:43 -0300

    saida de dados

--- saida_de_dados/src/com/saidadedados/Main.java
+++ saida_de_dados/src/com/saidadedados/Main.java
@@ -8,21 +8,29 @@ public class Main {
 
         int y = 32;
 
-        System.out.println("Para escrever uma mensagem é simples assim!!");
+        System.out.println("Writing in a program is this easy!!");
         System.out.println(y);
 
         double x = 10.35784;
 
         System.out.println(x);
 
-        /*O programa rodará usando padrões americanos,
-        * por exemplo, "10.36" ao inves de "10,36"
+        /*Running with american standards,
+        * like showing "10.36" instead "10,36"
         * */
         Locale.setDefault(Locale.US);
 
-        // printf: texto formatado
+        // printf: formated text
         System.out.printf("%.2f%n", x);
         System.out.printf("%.4f%n", x);
 
+        System.out.println("Concatenating: " + x + y); // the value isn't added, just concatenated
+
+        System.out.printf("Concatenating using format: %.2f%n", x);
+
+        String name = "Sarah";
+        byte age = 19;
+        double money = 9999.0;
+        System.out.printf("%s is %d years old and have $%.2f%n", name, age, money);
     }
 }
